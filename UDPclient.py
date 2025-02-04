a68c7672ae0a190fac82b536b43173ee3e473ad6
lioanys <lopezgarcialioanys@gmail.com>
2025-02-03 23:21:14 -0500

    Feature: receiving a file by chunks

--- UDPclient.py
+++ UDPclient.py
@@ -1,15 +1,29 @@
 import socket
 import pickle
+import pathlib
+import os
 serverName = '127.0.1.1'
 serverPort = 12000
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-message = 'lio'
+message = 'resources/lio.txt'
 serialized_data = pickle.dumps({'command': 'get', 'data': message.encode()})
 clientSocket.sendto(serialized_data, (serverName, serverPort))
 clientSocket.settimeout(2)
-try:
-    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-except:
-    print('time up')
-print(modifiedMessage.decode())
+new_file = pathlib.Path('resources/recived.txt').open( 'a+b')
+while True:
+    try:
+        serialized_response, serverAddress = clientSocket.recvfrom(2048)
+        response = pickle.loads(serialized_response)
+        if response['ok']:
+            data = response['data']
+            # os.system('clear')
+            print(data['message'].decode())
+            print(data['stream'])
+            new_file.write(data['stream'])
+        else:
+            data = response['data']
+            print(data['message'].decode())
+            break
+    except:
+        print('time up')
 clientSocket.close()
--- UDPserver.py
+++ UDPserver.py
@@ -1,5 +1,7 @@
 import socket
 import pickle
+import pathlib
+import math
 serverPort = 12000
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
@@ -13,8 +15,22 @@ while True:
     command = recived_object['command']
     data = recived_object['data']
     if command == 'get':
-        modifiedMessage = data.decode().upper()
-        serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+        file_path = pathlib.Path(data.decode())
+        if file_path.is_file():
+            file = file_path.open('rb').read()
+            max_size = 2
+            for i in range(0, len(file), max_size):
+                chunk = file[i:i + max_size]
+                response_msg = f'Downloading {math.floor((i + len(chunk)) * 100 / len(file))}%'
+                serialized_data = pickle.dumps({'ok': True, 'data': {'message': response_msg.encode(), 'stream': chunk}})
+                serverSocket.sendto(serialized_data, clientAddress)
+            response_msg = 'Transmition complete'
+            serialized_data = pickle.dumps({'ok': False, 'data': {'message': response_msg.encode(), 'stream': None}})
+            serverSocket.sendto(serialized_data, clientAddress)
+        else:
+            response_msg = 'not found'
+            serialized_data = pickle.dumps({'ok': False, 'data': {'message': response_msg.encode(), 'stream': None}})
+            serverSocket.sendto(serialized_data, clientAddress)
     elif command == 'set':
         print('not yet')
     else:
